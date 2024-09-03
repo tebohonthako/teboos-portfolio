3a558c61461a4a5959e6b3308e03dfc10571e40f
tebohonthako <tjaynthako@gmail.com>
2024-09-03 16:03:45 +0200

    backgroundCircles giving issues

---
+++ app/components/BackgroundCircles.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+export default function BackgroundCircles({}: Props) {
+  return (
+    <div>
+        <div className="border border-red-500 rounded-full h-[200px] w-[200px] mt-52 animate-ping"></div>
+        <div className="rounded-full border border-[#333333] h-[300px] w-[300px] absolute mt-52 "></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,22 +1,29 @@
 "use client";
 
-import React from 'react';
-import { Cursor, Typewriter, useTypewriter } from 'react-simple-typewriter'
+import React from "react";
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+// import {BackgroundCircles} from "../components/BackgroundCircles";
 
-type Props = {}
+
+type Props = {};
 
 export default function Hero({}: Props) {
-    const [text, count] = useTypewriter({
-        words: ["Hi, My Name is Teboho aka Teboo.dev", "Girl-who-loves-sushi.tsx 🍣", "ButLovesToCodeMore 💗👩🏽‍💻"],
-        loop: true,
-        delaySpeed: 2000,
-    })
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, My Name is Teboo",
+      "Girl-who-loves-sushi.tsx 🍣",
+      "ButLovesToCodeMore 💗👩🏽‍💻",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
     <div>
+        {/* <BackgroundCircles /> */}
+      <h1>
         <p>{text}</p>
-        <Cursor cursorColor="pink"/> 
-
+        <Cursor cursorColor="pink" />
+      </h1>
     </div>
-  )
+  );
 }
-
