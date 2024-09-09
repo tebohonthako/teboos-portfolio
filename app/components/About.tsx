b09133492185db29700b40a1bab95ec450e911c1
tebohonthako <tjaynthako@gmail.com>
2024-09-09 10:18:32 +0200

    use client added to about component fixed error for framer motion

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,3 +1,10 @@
+// "use client" directive is necessary when you want to indicate that a 
+// particular component should be rendered on the client-side instead of the server-side
+
+// N E X T   R E N D E R S C  O M P O N N E N T S  O N   S E R V E R   S I D E  4  S E O  +  P E R F O R M A N C E  
+
+'use client'   
+
 import { motion } from "framer-motion";
 import React from "react";
 
@@ -10,13 +17,15 @@ export default function About({}: Props) {
         About
       </h3>
 
-      {/* <motion.img 
+      <motion.img 
       initial={{x: -200,}}
-      wwhileInView={{ x:0,}}
-      src="/assets/tebz.jpg"/> 
-      */}
+      whileInView={{ x: 0,}}
+      viewport={{ once: true}}
+      src="/assets/teboooo.jpg"/> 
+     
 
-      <img src="/assets/teboooo.jpg" alt="" />
+      {/* <img src="/assets/teboooo.jpg" alt="" /> */}
     </div>
   );
 }
+
