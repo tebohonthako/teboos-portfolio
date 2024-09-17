91190b4fec4054c1f9b04127581228d0b6ef1d60
tebohonthako <tjaynthako@gmail.com>
2024-09-17 11:57:03 +0200

    skills section half done

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,12 +1,28 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-type Props = {};
+type Props = {
+  directionLeft?: boolean;
+};
 
-export default function Skill({}: Props) {
+export default function Skill({ directionLeft }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img></motion.img>
+      <motion.img
+        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        src="/assets/react.png"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+      ></motion.img>
+
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300">
+
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+
+      </div>
     </div>
   );
 }
