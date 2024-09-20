b0160716dace5b79f4660aea91b4bf133803eb29
tebohonthako <tjaynthako@gmail.com>
2024-09-20 04:12:08 +0200

    fixes

--- app/components/WorkExperience.tsx
+++ app/components/WorkExperience.tsx
@@ -18,17 +18,15 @@ export default function WorkExperience({}: Props) {
       transition={{
         duration: 1.5,
       }}
-      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center"
+      className="h-screen flex flex-col text-left md:flex-row max-w-full justify-center mx-auto items-center relative overflow-hidden"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory justify-center py-10">
         <ExperienceCard />
-        {/* <ExperienceCard /> */}
-       
-        
+        {/* Add more ExperienceCard components if needed */}
       </div>
     </motion.div>
   );
