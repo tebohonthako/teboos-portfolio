367b01caba2347b2991207e1a278a6f2599a18b9
tebohonthako <tjaynthako@gmail.com>
2024-09-20 10:01:04 +0200

    static

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    output: "export",  // <=== enables static files exports cause Github pages only hosts statis files
+};
 
 export default nextConfig;
