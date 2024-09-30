204892d27595d9b2fab47575555fa97a09cfcac8
tebohonthako <tjaynthako@gmail.com>
2024-09-30 10:12:49 +0200

    final commit

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
-
+import {images} from "./images"
 
 type Props = {
   directionLeft?: boolean;
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -11,17 +11,17 @@ export default function Skills({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center mx-auto items-center"
+      className="h-screen relative flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
-      {/* <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
-      </h3> */}
-      
-      {/* <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-300 text-sm">
-        Hover over a skill for current proficiency{" "}
-      </h3> */}
+      </h3>
 
-      <div className="grid grid-cols-4 gap-5 mt-12 xl:mt-0">
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-300 text-sm">
+        Hover over a skill for currency profiency{" "}
+      </h3>
+
+      <div className="grid grid-cols-4 gap-5">
         <Skill />
         <Skill />
         <Skill />
